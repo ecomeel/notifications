4c06af9a497516043f9ac3ae3ead1d5c79a303f6
ecomeel <ecomeel@gmail.com>
2023-12-24 18:02:08 +0300

    fixed a bug

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ function Layout() {
     return (
         <>
             <header className="notifications__header">
-                <h1 className="notifications__title">Уведовления</h1>
+                <h1 className="notifications__title">Уведомления</h1>
             </header>
             <Outlet />
         </>
